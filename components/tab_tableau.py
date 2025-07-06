7f77bbe2a5db98d4cf24efc3401ec70985fd6afb
seyoung hwang <vviyott@naver.com>
2025-07-06 13:00:40 +0900

    Update tab_tableau.py

--- components/tab_tableau.py
+++ components/tab_tableau.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 
 def create_market_dashboard():
     """미국 시장 진출 대시보드 UI 생성"""
-    # 설명 문구 추가
+    # 설명 문구
     st.info("""
     미국 식품 시장 동향을 시각화된 자료로 확인할 수 있습니다.\n
     모든 시각화 자료는 다운로드 기능을 제공합니다.""")
@@ -159,3 +159,56 @@ def create_market_dashboard():
             )
             
             st.caption("출처: [FDA Recall Database](https://www.fda.gov/safety/recalls-market-withdrawals-safety-alerts)")
+
+    # 네 번째 태블로 시각화
+    st.markdown("<h5 style='text-align: left;'># 리콜 등급(Class)별 발생 건수</h5>", unsafe_allow_html=True)
+
+    components.html(
+        """
+        <div class='tableauPlaceholder' id='viz_recall_class' style='position:relative; width:100%;'>
+        <noscript>
+            <a href='#'>
+            <img alt='리콜 등급(Class)별 발생 건수' src='https://public.tableau.com/static/images/fo/food_recall_class_01/1_1/1_rss.png' style='border: none' />
+            </a>
+        </noscript>
+        <object class='tableauViz'  style='display:none;'>
+            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+            <param name='embed_code_version' value='3' />
+            <param name='site_root' value='' />
+            <param name='name' value='food_recall_class_01/1_1' />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='static_image' value='https://public.tableau.com/static/images/fo/food_recall_class_01/1_1/1.png' />
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='ko-KR' />
+            <param name='filter' value='publish=yes' />
+        </object>
+        </div>
+        <script type='text/javascript'>
+            var divElement = document.getElementById('viz_recall_class');
+            var vizElement = divElement.getElementsByTagName('object')[0];
+            
+            if (divElement.offsetWidth > 800) {
+                vizElement.style.width='100%';
+                vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+            } else if (divElement.offsetWidth > 500) {
+                vizElement.style.width='100%';
+                vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+            } else {
+                vizElement.style.width='100%';
+                vizElement.style.height='727px';
+            }
+            
+            var scriptElement = document.createElement('script');
+            scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+            vizElement.parentNode.insertBefore(scriptElement, vizElement);
+        </script>
+        """,
+        height=440 # 세로 크기를 넉넉하게 확보
+    )
+    
+    st.caption("출처: [FDA Recall Database](https://www.fda.gov/safety/recalls-market-withdrawals-safety-alerts)")
